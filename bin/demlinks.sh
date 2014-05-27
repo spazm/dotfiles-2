c376b809b0c2a088b709b2bd4da416389541263f
Brandon Burton <brandon@inatree.org>
2014-05-27 11:32:02 -0700

    helper shell script for creating symlinks for stuff in dotfiles repos

---
+++ bin/demlinks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+$DOTFILES_HOME="$HOME/code/dotfiles"
+
+## manage symlinks for dotfiles
+
+# ~/bin/
+ln -s "$DOTFILES_HOME/bin" "$HOME/bin"
+
+# EMACS #
+ln -s "$DOTFILES_HOME/emacsd" "$HOME/.emacs.d"
+
+# MUTT #
+ln -s "$DOTFILES_HOME/muttrc" "$HOME/.muttrc"
+ln -s "DOTFILES_HOME/offlineimaprc" "$HOME/.offlineimaprc"
+ln -s "$DOTFILES_HOME/notmuch-config" "$HOME/.notmuch-config"
+ln -s "$DOTFILES_HOME/urlview" "$HOME/.urlview"
