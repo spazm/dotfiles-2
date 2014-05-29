99c090b3567ab46b8abb5fe2a46e20ecaf86066b
Brandon Burton <brandon@inatree.org>
2014-05-29 07:01:04 -0700

    * fixed typo in bin/demlinks.sh for mutt stuff * added symlink for zshrc * added zshrc

--- bin/demlinks.sh
+++ bin/demlinks.sh
@@ -12,6 +12,9 @@ ln -s "$DOTFILES_HOME/emacsd" "$HOME/.emacs.d"
 
 # MUTT #
 ln -s "$DOTFILES_HOME/muttrc" "$HOME/.muttrc"
-ln -s "DOTFILES_HOME/offlineimaprc" "$HOME/.offlineimaprc"
+ln -s "$DOTFILES_HOME/offlineimaprc" "$HOME/.offlineimaprc"
 ln -s "$DOTFILES_HOME/notmuch-config" "$HOME/.notmuch-config"
 ln -s "$DOTFILES_HOME/urlview" "$HOME/.urlview"
+
+# ZSH #
+ln -s "$DOTFILES_HOME/zshrc" "$HOME/.zshrc"
