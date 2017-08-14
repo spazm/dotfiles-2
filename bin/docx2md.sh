c0479fcca83e58724c7140d6f4208f672a0090bf
Brandon Burton <brandon@inatree.org>
2017-08-14 14:28:46 -0700

    Add check for pandoc binary

--- bin/docx2md.sh
+++ bin/docx2md.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ ! $(which pandoc) ]]; then
+  echo "Please install pandoc from https://pandoc.org/installing.html"
+  exit 1
+fi
+
 DOCNAME=$1
 DOCNAME_MD="$(echo $DOCNAME |  sed 's/docx/md/')"
 
