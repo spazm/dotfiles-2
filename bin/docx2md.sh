33ae4be2014e1d1d2d08bac6a9dab7eb2a3d85b5
Brandon Burton <brandon@inatree.org>
2017-08-14 14:26:34 -0700

    Some fixes
    - Handle file names with spaces
    - Be able to have the filename with `.docx` as the first
      argument

--- bin/docx2md.sh
+++ bin/docx2md.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 DOCNAME=$1
+DOCNAME_MD="$(echo $DOCNAME |  sed 's/docx/md/')"
 
 pandoc \
   --wrap=preserve \
   -f docx \
   -t markdown \
-  $DOCNAME.docx \
-  -o $DOCNAME.md
+  "$DOCNAME" \
+  -o "$DOCNAME_MD"
